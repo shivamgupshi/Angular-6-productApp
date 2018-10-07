64b190a86443451c3090e8492cdeb27da2bbc09b
Shivam Gupta <shivam.gupta@globant.com>
2018-10-07 21:50:25 +0530

    Adding guard for product detail list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from '../app/product-list/product-list.component';
 import { WelcomeComponent } from '../app/welcome/welcome.component';
 import { ProductDetailComponent } from '../app/product-detail/product-detail.component';
+import { ProductDetailGuard } from '../app/core/guard/product-detail.guard';
 
 const routes: Routes = [
   { path: 'product', component: ProductListComponent },
   { path: 'welcome', component: WelcomeComponent },
-  { path: 'product/:id', component: ProductDetailComponent },
+  { path: 'product/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
-
 
 ];
 
